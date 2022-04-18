209014f835808e3d6e3cc45b4b66168d5ad7a2df
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-18 20:03:48 -0300

    code fix component activities

--- src/app/features/backoffice/activities/activities.component.ts
+++ src/app/features/backoffice/activities/activities.component.ts
@@ -1,73 +1,76 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { ICategory } from './activities.interface';
-import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import {
+	FormBuilder,
+	FormControl,
+	FormGroup,
+	Validators,
+} from "@angular/forms";
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { ICategory } from "./activities.interface";
+import { ChangeEvent } from "@ckeditor/ckeditor5-angular/ckeditor.component";
 
 @Component({
-  selector: 'app-activities',
-  templateUrl: './activities.component.html',
-  styleUrls: ['./activities.component.scss']
+	selector: "app-activities",
+	templateUrl: "./activities.component.html",
+	styleUrls: ["./activities.component.scss"],
 })
 export class ActivitiesComponent implements OnInit {
+	constructor(private http: HttpClient, private fb: FormBuilder) {}
 
-  constructor(private http: HttpClient, private fb: FormBuilder) {
+	ngOnInit(): void {}
 
-   }
+	@Input() activitySelected: ICategory = {
+		id: 0,
+		name: "",
+		description: "",
+		pathImage: "",
+	};
 
-  ngOnInit(): void {
-  }
+	submitted = false;
+	activityForm = new FormGroup({
+		name: new FormControl(
+			this.activitySelected != undefined ? this.activitySelected.name : "",
+			[Validators.required]
+		),
+		description: new FormControl(
+			this.activitySelected != undefined
+				? this.activitySelected.description
+				: ""
+		),
+		image: new FormControl(
+			this.activitySelected != undefined ? this.activitySelected.pathImage : "",
+			Validators.required
+		),
+	});
 
-  category:ICategory= {id:0, name:"", description: "", pathImage: ""}
+	// --------Config CKEditor-------
+	public Editor = ClassicEditor;
+	public editorData =
+		this.activitySelected != undefined ? this.activitySelected.description : "";
+	public config = {
+		placeholder: "Ingrese la descripcion de la actividad",
+	};
+	// ------------------------------
 
-  @Input () activitySelected: ICategory | undefined;
+	get activityFormControl() {
+		return this.activityForm.controls;
+	}
 
+	onChange({ editor }: ChangeEvent) {
+		const data = editor.getData();
+		this.activityForm.value.description = data;
+	}
 
+	onFileSelected(event: any) {
+		let selectedFile = <File>event.target.files;
+	}
 
-  submitted = false;
-  activityForm = new FormGroup({
-    name: new FormControl(this.category!=undefined?this.category.name: '', [Validators.required]),
-    description: new FormControl(this.category!=undefined?this.category.description : ''),
-    image: new FormControl(this.category!=undefined?this.category.pathImage: '', Validators.required)
-  });
+	submitActivity() {
+		this.submitted = true;
+		if (this.activityForm.valid) {
+		}
+	}
 
-  // --------Config CKEditor-------
-  public Editor = ClassicEditor;
-  public editorData = this.category!=undefined?this.category.description : '';
-  public config = {
-    placeholder: 'Ingrese la descripcion de la actividad'
-  }
-  // ------------------------------
-
-  get activityFormControl(){
-    return this.activityForm.controls;
-  }
-
-  onChange({editor}:ChangeEvent){
-    const data = editor.getData();
-    this.activityForm.setValue({description:'data'});
-
-  }
-
-  onFileSelected(event: any){
-    let selectedFile=<File> event.target.files;
-    console.log(selectedFile);
-  }
-
-  submitActivity(){
-    this.submitted=true;
-    if (this.activityForm.valid) {
-      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
-      console.table(this.activityForm.value);
-    }
-  }
-
-
-  cancelAction(){
-
-  }
-
-
+	cancelAction() {}
 }
