4d5ff3e586e61be9ee3fba6c4fbd852c234ba732
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-19 20:22:58 -0300

    add imports and declarations in public module

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -2,14 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PrimengModule } from '../../core/utils/primeng/primeng.module';
 import { HomeComponent } from './pages/home/home.component';
+import { FormcontactComponent } from './formcontact/formcontact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FormcontactComponent
   ],
   imports: [
     CommonModule,
-    PrimengModule
+    PrimengModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class PublicModule { }
