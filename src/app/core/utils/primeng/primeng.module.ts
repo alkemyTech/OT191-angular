fde4f8f26c5ad837b3106bbab6fbae4ff0be2516
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-19 20:25:48 -0300

    added prime ng models

--- src/app/core/utils/primeng/primeng.module.ts
+++ src/app/core/utils/primeng/primeng.module.ts
@@ -1,9 +1,15 @@
 import {ButtonModule} from 'primeng/button';
 import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputMaskModule} from 'primeng/inputmask';
 
 @NgModule({
   exports: [ 
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputMaskModule
   ]
 })
 export class PrimengModule { }
