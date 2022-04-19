d3768474da6a4437eed0a0b90ed127866ad236eb
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-18 22:54:18 -0300

    Code fixes

--- src/app/features/backoffice/categories/categories.component.ts
+++ src/app/features/backoffice/categories/categories.component.ts
@@ -1,49 +1,63 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { ICategory } from './categories.interface';
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { ChangeEvent } from "@ckeditor/ckeditor5-angular/ckeditor.component";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent{
 
   constructor(private http: HttpClient, private fb: FormBuilder) {   }
 
+  @Input() categorySelected: ICategory ={
+    id:0,
+    name:"",
+    description:"",
+    pathImage: "",
+  }
   submitted = false;
   categoryForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    description: new FormControl('', Validators.required),
-    image: new FormControl('', Validators.required)
+    name: new FormControl(this.categorySelected.name!=""? this.categorySelected.name: "", [Validators.required, Validators.minLength(4)]),
+    description: new FormControl(this.categorySelected.description!=""? this.categorySelected.description: "", Validators.required),
+    image: new FormControl(this.categorySelected.pathImage!=''? this.categorySelected.pathImage: "", Validators.required)
   });
 
-  description: string="";
-  name: string="";
-
-  ngOnInit(): void {  }
+	// --------Config CKEditor-------
+	public Editor = ClassicEditor;
+	public editorData =
+		this.categorySelected != undefined ? this.categorySelected.description : "";
+	public config = {
+		placeholder: "Ingrese la descripcion de la categoria",
+	};
+	// ------------------------------
 
   get categoryFormControl(){
     return this.categoryForm.controls;
   }
+
+	onChange({ editor }: ChangeEvent) {
+		const data = editor.getData();
+		this.categoryForm.value.description = data;
+	}
   
   onFileSelected(event: any){
     let selectedFile=<File> event.target.files;
-    console.log(selectedFile);
   }
 
   submitCategory(){
     this.submitted=true;
     if (this.categoryForm.valid) {
-      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
-      console.table(this.categoryForm.value);
+
     }
   }
 
 
   cancelAction(){
-    console.log("asdas");
     console.log(this.categoryFormControl.name.errors);
   }
 }
