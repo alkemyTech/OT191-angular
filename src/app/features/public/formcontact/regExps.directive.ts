efaa0a834993c03d4e7447f1023f0200b1dbcff3
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-19 20:23:30 -0300

    add customer validators in regExps directive

---
+++ src/app/features/public/formcontact/regExps.directive.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function mailValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formatMail = nameRe.test(control.value);
+      return formatMail? null: {mailFormat: {value: control.value}} ;
+    };
+  }
+
+export function phoneValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const formatMail = nameRe.test(control.value);
+    return formatMail? null: {phoneFormat: {value: control.value}} ;
+  };
+}
