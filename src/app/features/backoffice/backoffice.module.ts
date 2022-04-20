d214ab59abb0fecd9653b1417c5a00d08acc4e7e
Emanuel Ibarrola <emanuel.ibarrola3001@gmail.com>
2022-04-20 12:36:00 -0300

    init ot191-35

--- src/app/features/backoffice/backoffice.module.ts
+++ src/app/features/backoffice/backoffice.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PrimengModule } from '../../core/utils/primeng/primeng.module';
 import { SlidesComponent } from './pages/slides/slides.component';
+import { EditionFormComponent } from './pages/edition-form/edition-form.component';
 
 @NgModule({
   declarations: [
-    SlidesComponent
+    SlidesComponent,
+    EditionFormComponent
   ],
   imports: [
     CommonModule,
