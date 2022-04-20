d9dba754c50d0fcd9d1e9cd0acba448b8e6a788a
Emanuel Ibarrola <emanuel.ibarrola3001@gmail.com>
2022-04-20 15:58:37 -0300

    form editor  primera vercion lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { AppComponent } from './app.component';
-import { AuthModule } from './features/auth/auth.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { CoreModule } from './core/core.module';
-import { FeaturesModule } from './features/features.module';
-import { NgModule } from '@angular/core';
-import { PrimengModule } from './core/utils/primeng/primeng.module';
+import { AppComponent } from "./app.component";
+import { AuthModule } from "./features/auth/auth.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { CoreModule } from "./core/core.module";
+import { FeaturesModule } from "./features/features.module";
+import { NgModule } from "@angular/core";
+import { PrimengModule } from "./core/utils/primeng/primeng.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    CoreModule,
-    FeaturesModule, 
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, FeaturesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
