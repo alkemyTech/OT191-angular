02149b129fa14f1fc2367fb08eaa8dd5fadced3d
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-20 16:20:15 -0300

    fixes for features module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from "@angular/router";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
+import { BackofficeModule } from "./backoffice/backoffice.module";
 
 
 @NgModule({
@@ -23,8 +24,6 @@ import { UserFormComponent } from "./pages/users/user-form/user-form.component";
     SlidesFormComponent,
     TestimonialFormComponent,
     UserFormComponent,
-    
-    routingComponents,
   ],
   exports: [
     ActivityFormComponent,
@@ -35,9 +34,8 @@ import { UserFormComponent } from "./pages/users/user-form/user-form.component";
     SlidesFormComponent,
     TestimonialFormComponent,
     UserFormComponent,
-    
-    routingComponents,
-    RouterModule
+    RouterModule,
+    BackofficeModule
   ],
   imports: [
     CommonModule, 
