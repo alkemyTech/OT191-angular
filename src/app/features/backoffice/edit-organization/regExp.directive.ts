103a0c1a3723fe835e03ff012798012c5ff3f442
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-21 15:41:27 -0300

    urlValidator modified

--- src/app/features/backoffice/edit-organization/regExp.directive.ts
+++ src/app/features/backoffice/edit-organization/regExp.directive.ts
@@ -1,19 +1,17 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
-export function urlValidator(site:string): ValidatorFn {
-    const nameRe= new RegExp("[a-zA-Z0-9./]{1,50}")
+export function urlValidator(site: string): ValidatorFn {
+	const nameRe = new RegExp("[a-zA-Z0-9./]{1,50}");
 	return (control: AbstractControl): ValidationErrors | null => {
-        if (String(control.value).includes(site)){
-            const start= String(control.value).indexOf('.com/')+4;
-            const end= String(control.value).length;
-            const user= String(control.value).substring(start,end);
-            console.log(user);
-            const formatUrl = nameRe.test(user);
-            console.log(formatUrl);
-            return formatUrl ? null : { formatUrl: { value: control.value } };
-        }else{
-            return { formatUrl: { value: control.value } }
-        }
-
+		if (String(control.value).includes(site)) {
+			const start = String(control.value).indexOf(".com/") + 4;
+			console.log(start);
+			const end = String(control.value).length;
+			const user = String(control.value).substring(start, end);
+			const formatUrl = nameRe.test(user);
+			return formatUrl ? null : { formatUrl: { value: control.value } };
+		} else {
+			return { formatUrl: { value: control.value } };
+		}
 	};
 }
