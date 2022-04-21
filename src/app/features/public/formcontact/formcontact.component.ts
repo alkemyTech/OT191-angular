54a277c930735fa98e9f66758d68dfac3a89b113
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-21 17:58:06 -0300

    formatted code

--- src/app/features/public/formcontact/formcontact.component.ts
+++ src/app/features/public/formcontact/formcontact.component.ts
@@ -1,54 +1,52 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 import {
-  AbstractControl,
+	AbstractControl,
 	FormBuilder,
 	FormControl,
 	FormGroup,
 	Validators,
 } from "@angular/forms";
-import { mailValidator } from './regExps.directive';
+import { mailValidator } from "./regExps.directive";
 
 @Component({
-  selector: 'app-formcontact',
-  templateUrl: './formcontact.component.html',
-  styleUrls: ['./formcontact.component.scss']
+	selector: "app-formcontact",
+	templateUrl: "./formcontact.component.html",
+	styleUrls: ["./formcontact.component.scss"],
 })
 export class FormcontactComponent {
+	constructor(private http: HttpClient, private fb: FormBuilder) {}
 
-  constructor(private http: HttpClient, private fb: FormBuilder) { }
+	public autoResize: boolean = true;
+	submitted: boolean = false;
 
-  public autoResize: boolean = true;
-  submitted:boolean =false;
+	contactForm = new FormGroup({
+		name: new FormControl("", [Validators.required]),
+		phone: new FormControl("", [Validators.required]),
+		mail: new FormControl("", [
+			Validators.required,
+			Validators.email,
+			mailValidator(/^[a-z]+[a-z0-9._-]+@[a-z]+\.[a-z.]{2,5}$/),
+		]),
+		message: new FormControl("", [Validators.required]),
+	});
 
-  contactForm= new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    phone: new FormControl('', [Validators.required]),
-    mail: new FormControl('', [Validators.required, Validators.email, mailValidator(/^[a-z]+[a-z0-9._-]+@[a-z]+\.[a-z.]{2,5}$/)]),
-    message: new FormControl('', [Validators.required])
-  })
-  
 	get contactFormControl() {
 		return this.contactForm.controls;
 	}
 
-  validateControlForSpacing(control: AbstractControl){
-    if(control.valid || control.untouched){
-      return true
-    }else{
-      return false
-    }
-  }
+	validateControlForSpacing(control: AbstractControl) {
+		if (control.valid || control.untouched) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  validateControlForText(control: AbstractControl){
-    return (control.touched || this.submitted) && control.invalid
-  }
+	validateControlForText(control: AbstractControl) {
+		return (control.touched || this.submitted) && control.invalid;
+	}
 
-
-  submitContact(){
-    console.log(this.contactForm.value)
-  }
-  cancelAction(){
-  }
-
+	submitContact() {}
+	cancelAction() {}
 }
--- src/app/features/public/formcontact/regExps.directive.ts
+++ src/app/features/public/formcontact/regExps.directive.ts
@@ -1,15 +1,15 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function mailValidator(nameRe: RegExp): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const formatMail = nameRe.test(control.value);
-      return formatMail? null: {mailFormat: {value: control.value}} ;
-    };
-  }
+	return (control: AbstractControl): ValidationErrors | null => {
+		const formatMail = nameRe.test(control.value);
+		return formatMail ? null : { mailFormat: { value: control.value } };
+	};
+}
 
 export function phoneValidator(nameRe: RegExp): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const formatMail = nameRe.test(control.value);
-    return formatMail? null: {phoneFormat: {value: control.value}} ;
-  };
+	return (control: AbstractControl): ValidationErrors | null => {
+		const formatMail = nameRe.test(control.value);
+		return formatMail ? null : { phoneFormat: { value: control.value } };
+	};
 }
