1ba08e1f2c3beb6e163860ea5cfb74585192586d
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-21 15:40:49 -0300

    code fixes in edit-organization component

--- src/app/features/backoffice/edit-organization/edit-organization.component.ts
+++ src/app/features/backoffice/edit-organization/edit-organization.component.ts
@@ -8,57 +8,79 @@ import {
 	Validators,
 } from "@angular/forms";
 import { urlValidator } from "./regExp.directive";
-
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { ChangeEvent } from "@ckeditor/ckeditor5-angular/ckeditor.component";
+import { IOrganization } from "../backoffice.interface";
 @Component({
 	selector: "app-edit-organization",
 	templateUrl: "./edit-organization.component.html",
 	styleUrls: ["./edit-organization.component.scss"],
 })
 export class EditOrganizationComponent {
-	constructor() {}
+	constructor(private http: HttpClient) {}
 
-  // @Input() activitySelected: IOrganization = {
-	// 	id: 0,
-	// 	name: "",
-	// 	description: "",
-	// 	pathImage: "",
-	// };
+	@Input() organizationData: IOrganization = {
+		name: "",
+		logo: "",
+		shortDescription: "",
+		longDescription: "",
+		links: {
+			facebook: "",
+			instagram: "",
+			twitter: "",
+		},
+	};
 
 	public autoResize: boolean = true;
 	submitted: boolean = false;
+	imageSource: any;
+	logoEmpty = false;
 	organizationForm = new FormGroup({
-		name: new FormControl("", [Validators.required]),
+		name: new FormControl(this.organizationData.name, [Validators.required]),
 		logo: new FormControl("", [Validators.required]),
-		shortDescription: new FormControl("", [Validators.required]),
-		longDescription: new FormControl("", [Validators.required]),
+		shortDescription: new FormControl(this.organizationData.shortDescription, [
+			Validators.required,
+		]),
+		longDescription: new FormControl(this.organizationData.longDescription, [
+			Validators.required,
+		]),
 		links: new FormGroup({
-			facebook: new FormControl("", [
+			facebook: new FormControl(this.organizationData.links.facebook, [
 				Validators.required,
-				urlValidator("www.facebook.com"),
+				urlValidator("www.facebook.com/"),
 			]),
-			instagram: new FormControl("", urlValidator("www.instagram.com")),
-			twitter: new FormControl("", urlValidator("www.twitter.com")),
+			instagram: new FormControl(
+				this.organizationData.links.instagram,
+				urlValidator("www.instagram.com/")
+			),
+			twitter: new FormControl(
+				this.organizationData.links.twitter,
+				urlValidator("www.twitter.com/")
+			),
 		}),
 	});
 
-
 	// // --------Config CKEditor-------
-	// public Editor = ClassicEditor;
-	// public editorData =
-	// 	this.activitySelected != undefined ? this.organizationForm.longDescription : "";
-	// public config = {
-	// 	placeholder: "Ingrese la descripcion de la actividad",
-	// };
-	// // ------------------------------
+	public Editor = ClassicEditor;
+	public editorData =
+		this.organizationData != undefined
+			? this.organizationData.longDescription
+			: "";
+	public config = {
+		placeholder: "Descripcion corta de la organizacion",
+	};
+	// ------------------------------
 
-
-  get organizationFormControl() {
+	get organizationFormControl() {
 		return this.organizationForm.controls;
 	}
 	get organizationFormLinksControl() {
 		return (this.organizationForm.get("links") as FormGroup).controls;
 	}
-
+	onChange({ editor }: ChangeEvent) {
+		const data = editor.getData();
+		this.organizationForm.value.longDescription = data;
+	}
 	validateControlForSpacing(control: AbstractControl) {
 		if (control.valid || control.untouched) {
 			return true;
@@ -70,13 +92,31 @@ export class EditOrganizationComponent {
 	validateControlForText(control: AbstractControl) {
 		return (control.touched || this.submitted) && control.invalid;
 	}
+	nothingSelected() {
+		this.organizationFormControl.logo.setValue("");
+		this.logoEmpty = true;
+	}
+	onFileSelected(e: any): void {
+		if (e.target.files && e.target.files[0]) {
+			const imageFile = e.target.files[0];
+			const fileReader = new FileReader();
 
-	onFileSelected(event: any) {
-		let selectedFile = <File>event.target.files;
+			fileReader.onload = () => {
+				this.logoEmpty = false;
+				return this.organizationFormControl.logo.setValue(fileReader.result);
+			};
+			fileReader.readAsDataURL(imageFile);
+		}
 	}
 	submitOrganization() {
-		console.log("hola");
-		console.log(this.organizationForm.value);
+		console.log("boton submit");
+		console.log(this.organizationFormControl.logo.value);
+		if (this.organizationFormControl.logo.value == "") {
+			this.organizationForm.controls.logo.setValue(this.organizationData.logo);
+		}
+		if (this.organizationForm.valid) {
+			console.log(this.organizationForm.value);
+		}
 	}
 	cancelAction() {}
 }
