b9b09fa5cd2793b735f231b26824a44fb2071cb0
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-22 16:22:54 -0300

    code fix

--- src/app/shared/publicservices/publicapiService.service.ts
+++ src/app/shared/publicservices/publicapiService.service.ts
@@ -1,27 +1,13 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
-
 export class PublicapiServiceService {
+	constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { } 
-
-  params= new HttpParams();
-  Api="https://reqres.in/api/users";
-
-  get(destinationRoute: string, id:number|null){
-    this.params=this.params.append('type', 'all-meat')
-    this.params=this.params.append('sentences', '1')
-    this.params=this.params.append('format', 'text')
-    const options={
-      params:this.params
-    }
-    return this.http.get(this.Api+'/'+id)
-  } 
-
-  
-  
+	getDataApi(destinationRoute: string, id: number | null) {
+		return this.http.get(destinationRoute + "/" + id);
+	}
 }
