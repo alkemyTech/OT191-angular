edb86ce8bba518efa3bffbce8691429037ab7ebb
Javernaver <Javernaver@gmail.com>
2022-04-21 23:42:32 -0400

    servicios api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { CoreModule } from './core/core.module';
-import { FeaturesModule } from './features/features.module';
-import { NgModule } from '@angular/core';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { CoreModule } from "./core/core.module";
+import { FeaturesModule } from "./features/features.module";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, FeaturesModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    FeaturesModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/features/backoffice/services/private-api.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { BaseApiService } from "../../shared/services/base-api.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PrivateApiService extends BaseApiService {
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  options() {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("token") || "",
+      }),
+    };
+  }
+
+  public get<T>(path: string): Observable<T> {
+    this.options();
+    return super.get<T>(path);
+  }
+
+  public post<T>(path: string, body: any): Observable<T> {
+    this.options();
+    return super.post<T>(path, body);
+  }
+
+  public delete<T>(path: string, id: string = ''): Observable<T> {
+    this.options();
+    return super.delete<T>(path  + '/' + id);
+  }
+}
