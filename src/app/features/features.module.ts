21810e54e8fcc777a7eee3e72d631adc96dbe87b
stefanoracke <stefanoracke@gmail.com>
2022-04-22 15:02:15 -0300

    change body and header

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,31 +1,23 @@
-import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
+import { AppRoutingModule } from "./app-routing.module";
 import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
 import { CommonModule } from "@angular/common";
-
+import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
+import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { NgModule } from "@angular/core";
 import { PrimengModule } from '../core/utils/primeng/primeng.module';
-
+import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
 import { RouterModule } from "@angular/router";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
+import { BackofficeModule } from "./backoffice/backoffice.module";
+import { PublicModule } from "./public/public.module";
 import { SharedModule } from '../shared/shared.module';
 
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { LoginFormComponent } from "./auth/pages/login-form/login-form.component";
-import { RegisterFormComponent } from "./auth/pages/register-form/register-form.component";
-import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 
 @NgModule({
   declarations: [
-    
-    routingComponents,
-  ],
-  exports: [
-    ReactiveFormsModule,
-    FormsModule,
-    routingComponents,
     ActivityFormComponent,
     LoginFormComponent,
     RegisterFormComponent,
@@ -35,15 +27,26 @@ import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
     TestimonialFormComponent,
     UserFormComponent,
   ],
-  
+  exports: [
+    ActivityFormComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    CategoriesFormComponent,
+    NewsFormComponent,
+    SlidesFormComponent,
+    TestimonialFormComponent,
+    UserFormComponent,
+    RouterModule,
+    BackofficeModule,
+    PublicModule,
+    RouterModule
+  ],
   imports: [
     CommonModule, 
     AppRoutingModule, 
     RouterModule,
     PrimengModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule
+    SharedModule,
 ],
 })
 export class FeaturesModule {}
