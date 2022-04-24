c69a820ccb2bd8cee7cfa4767573ddd278b4f2e8
Daniel Medina <k46468@frro.utn.edu.ar>
2022-04-24 19:19:35 -0300

    add confirmMessageService and messageService in providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { FeaturesModule } from "./features/features.module";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "./shared/shared.module";
 import { AboutModule } from "./About/about.module";
+import { ConfirmationService, MessageService } from "primeng/api";
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, BrowserAnimationsModule, CoreModule, FeaturesModule, SharedModule, AboutModule],
-	providers: [],
+	providers: [ConfirmationService, MessageService],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
