1695b9602be0b24dec84eef2ac839d6cba46d7fa
Javier del Canto Montenegro <38194420+Javernaver@users.noreply.github.com>
2022-04-25 14:23:03 -0400

    Ot191 83 (#25)
    * servicios api
    
    * pruebas servicio
    
    * fixes
    
    * fix duplicate import
    
    * services api / estructura api / actualizacion de rutas
    
    * Home API partial

--- src/app/features/public/services/providers/home-provider.service.ts
+++ src/app/features/public/services/providers/home-provider.service.ts
@@ -1,9 +1,13 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+
 import { PublicApiService } from "../public-api.service";
+import { News } from "../../../../core/models/news.model";
+import { Organization } from "../../../../core/models/organization.model";
 import { Slide } from "../../../../core/models/slides.model";
+import { Testimonial } from "../../../../core/models/testimonial.model";
 
 @Injectable({
   providedIn: "root",
@@ -23,6 +27,30 @@ export class HomeProviderService {
     return this.publicApiService.get("/slides").pipe(
       map((res: any) => {
         return res.data.filter((slide: any) => slide.order !== null);
+      })
+    );
+  }
+
+  public getAllTestimonials(): Observable<Testimonial[]> {
+    return this.publicApiService.get("/testimonials").pipe(
+      map((res: any) => {
+        return res.data;
+      })
+    );
+  }
+
+  public getAllNews(): Observable<News[]> {
+    return this.publicApiService.get("/news").pipe(
+      map((res: any) => {
+        return res.data;
+      })
+    );
+  }
+
+  public getOrganization(): Observable<Organization[]> {
+    return this.publicApiService.get("/organization").pipe(
+      map((res: any) => {
+        return res.data;
       })
     );
   }
