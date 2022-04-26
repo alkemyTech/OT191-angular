1181791755237110f385d5ea598cd468e506905f
Javernaver <Javernaver@gmail.com>
2022-04-25 21:11:30 -0400

    format document

--- src/app/features/backoffice/components/table-users/table-users.component.ts
+++ src/app/features/backoffice/components/table-users/table-users.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from "@angular/core";
 
-import { User } from 'src/app/core/models/user.model';
+import { User } from "src/app/core/models/user.model";
 
-import { MessageService } from 'primeng/api';
-import { Table } from 'primeng/table';
+import { MessageService } from "primeng/api";
+import { Table } from "primeng/table";
 
-import { UserProviderService } from '../../services/providers/user-provider.service';
-import { AlertService } from '../../../../core/services/alert.service';
+import { UserProviderService } from "../../services/providers/user-provider.service";
+import { AlertService } from "../../../../core/services/alert.service";
 
 @Component({
-  selector: 'app-table-users',
-  templateUrl: './table-users.component.html',
-  styleUrls: ['./table-users.component.scss'],
+  selector: "app-table-users",
+  templateUrl: "./table-users.component.html",
+  styleUrls: ["./table-users.component.scss"],
   providers: [MessageService],
 })
 export class TableUsersComponent {
@@ -26,17 +26,17 @@ export class TableUsersComponent {
   submitted: boolean = false;
 
   statuses: any[] = [
-    { label: 'Normal', value: 1 },
-    { label: 'Administrador', value: 2 },
+    { label: "Normal", value: 1 },
+    { label: "Administrador", value: 2 },
   ];
 
-  @ViewChild('dt') dt: Table | undefined;
+  @ViewChild("dt") dt: Table | undefined;
   // filtro para el buscador de una columna
   applyFilter($event: any, stringVal: any) {
     this.dt!.filter(
       ($event.target as HTMLInputElement).value,
       stringVal,
-      'contains'
+      "contains"
     );
   }
 
@@ -54,16 +54,16 @@ export class TableUsersComponent {
   deleteSelectedUsers() {
     this.alerts
       .alertQuestion(
-        'Borrar',
-        '¿Seguro que quieres borrar todos los usuarios marcados?',
-        'warning'
+        "Borrar",
+        "¿Seguro que quieres borrar todos los usuarios marcados?",
+        "warning"
       )
       .then(async (result) => {
         if (result.isConfirmed) {
           this.messageService.add({
-            severity: 'success',
-            summary: 'Éxito',
-            detail: 'Se han eliminado los usuarios',
+            severity: "success",
+            summary: "Éxito",
+            detail: "Se han eliminado los usuarios",
           });
 
           this.users = this.users!.filter(
@@ -80,9 +80,9 @@ export class TableUsersComponent {
   deleteUser(user: User) {
     this.alerts
       .alertQuestion(
-        'Borrar',
-        '¿Seguro que quieres borrar este usuario?',
-        'warning'
+        "Borrar",
+        "¿Seguro que quieres borrar este usuario?",
+        "warning"
       )
       .then(async (result) => {
         if (result.isConfirmed) {
@@ -92,17 +92,17 @@ export class TableUsersComponent {
           this.userP.deleteUser(user.id.toString()).subscribe({
             next: (data) => {
               this.messageService.add({
-                severity: 'error',
-                summary: 'Se eliminó',
-                detail: 'Se ha eliminado el usuario',
+                severity: "error",
+                summary: "Se eliminó",
+                detail: "Se ha eliminado el usuario",
                 life: 3000,
               });
             },
             error: (err) => {
               this.alerts.alertNotification(
-                'Error',
+                "Error",
                 err.error.message,
-                'error'
+                "error"
               );
               return;
             },
@@ -124,14 +124,14 @@ export class TableUsersComponent {
     this.userP.updateUser(this.user.id!.toString(), this.user).subscribe({
       next: (data) => {
         this.messageService.add({
-          severity: 'success',
-          summary: 'Éxito',
-          detail: 'Se ha actualizado el usuario',
+          severity: "success",
+          summary: "Éxito",
+          detail: "Se ha actualizado el usuario",
           life: 3000,
         });
       },
       error: (err) => {
-        this.alerts.alertNotification('Error', err.error.message, 'error');
+        this.alerts.alertNotification("Error", err.error.message, "error");
         return;
       },
     });
