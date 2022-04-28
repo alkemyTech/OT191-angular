ecc2afefd799a56862163868c194dac26be47bbd
Daniel-Mdn <danielmedina012@gmail.com>
2022-04-28 11:58:27 -0300

    Environment example created

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,8 @@
+export const environment={
+    production: false,
+    apiURL: 'https://ongapi.alkemy.org/api',
+    activitiesURL:'/activities',
+    listActivities:'/activities',
+    activityCreate:'/activities/create',
+    activityEdit:'/activities/create/:id',
+};
