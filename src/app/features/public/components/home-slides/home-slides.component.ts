53a3f273755b79adc27c88468b136e9bc2bf6047
stefanoracke <stefanoracke@gmail.com>
2022-05-02 16:58:56 -0300

    Agregando componente slide para home

--- src/app/features/public/components/home-slides/home-slides.component.ts
+++ src/app/features/public/components/home-slides/home-slides.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Slide } from 'src/app/core/models/slides.model';
 
 @Component({
-  selector: 'app-home-slides',
-  templateUrl: './home-slides.component.html',
-  styleUrls: ['./home-slides.component.scss']
+  selector: "app-home-slides",
+  templateUrl: "./home-slides.component.html",
+  styleUrls: ["./home-slides.component.scss"],
 })
 export class HomeSlidesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  slides!: Slide[];
 
-  ngOnInit(): void {
-  }
+  slides2 = [
+    {
+      name: "First slide",
+      description: "some description",
+      image: "../../../../../assets/img/auth/login.jpg",
+    },
+    {
+      name: "Second slide",
+      description: "some description",
+      image: "../../../../../assets/img/auth/login.jpg",
+    },
+    {
+      name: "third slide",
+      description: "some description",
+      image: "../../../../../assets/img/auth/login.jpg",
+    },
+  ];
 
+  ngOnInit(): void {}
 }
