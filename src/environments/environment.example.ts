2133f5dbaa341a4c827cd59ee85e990c4e3144a1
Daniel-Mdn <danielmedina012@gmail.com>
2022-05-06 11:42:33 -0300

    enviroment.example deleted

--- src/environments/environment.example.ts
+++
@@ -1,8 +0,0 @@
-export const environment={
-    production: false,
-    apiURL: 'https://ongapi.alkemy.org/api',
-    activitiesURL:'/activities',
-    listActivities:'/activities',
-    activityCreate:'/activities/create',
-    activityEdit:'/activities/create/:id',
-};
