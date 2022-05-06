e4ca4d20a478582e096089e0245db3959280c747
Daniel-Mdn <danielmedina012@gmail.com>
2022-05-06 11:42:14 -0300

    code fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,4 @@ export const environment = {
   production: true,
   apiURL: 'https://ongapi.alkemy.org/api',
   activitiesURL:'/activities',
-  listActivities:'/activities',
-  activityCreate:'/activities/create',
-  activityEdit:'/activities/create/:id',
 };
