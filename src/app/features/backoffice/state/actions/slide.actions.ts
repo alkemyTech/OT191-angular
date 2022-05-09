c0071a4656b5dcd885e66c4f8d851afa34a42734
stefanoracke <stefanoracke@gmail.com>
2022-05-09 10:45:04 -0300

    adding actions

---
+++ src/app/features/backoffice/state/actions/slide.actions.ts
@@ -0,0 +1,19 @@
+import { Slide } from "src/app/core/models/slides.model";
+import { createAction, props } from "@ngrx/store";
+
+export const loadingSlides = createAction("[Load Slides] Loading Slides");
+
+export const loadedSlides = createAction(
+  "[Load Slides] Loaded succes",
+  props<{ slides: Slide[] }>()
+);
+
+export const editSlide = createAction(
+    "[Edit Slide] Edited Slide",
+    props<{slide: Slide}>()
+);
+
+
+export const deleteSlides = createAction(
+    "[Delete Slide] Selected Slides deleted"
+);
