aca558f8274cfd27af9ee7d71978846490ac9b9a
Daniel-Mdn <danielmedina012@gmail.com>
2022-05-12 16:09:10 -0300

    member-list component created

---
+++ src/app/features/public/pages/about/member-list/member-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { IMember } from "src/app/core/models/member.model";
+import { MembersService } from "../../../services/members/members.service";
+
+@Component({
+	selector: "app-member-list",
+	templateUrl: "./member-list.component.html",
+	styleUrls: ["./member-list.component.scss"],
+})
+export class MemberListComponent {
+	@Input() memberList: IMember[] = [];
+	constructor(memberService: MembersService) {
+		memberService.getMembers("/members", null).subscribe({
+			next: (response) => {
+        console.log(response)
+				this.memberList = <IMember[]>response.data;
+			},
+			error: (error) => {
+				console.log(error);
+			},
+		});
+	}
+}
