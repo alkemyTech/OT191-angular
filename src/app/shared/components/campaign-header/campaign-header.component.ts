5ceb06e8cb1e4c7fbd12b7289ed12535739dc1b5
Daniel-Mdn <danielmedina012@gmail.com>
2022-05-12 12:59:42 -0300

    media queries and campaign images added

--- src/app/shared/components/campaign-header/campaign-header.component.ts
+++ src/app/shared/components/campaign-header/campaign-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-campaign-header',
@@ -12,4 +12,7 @@ export class CampaignHeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input() logoCamp:string="";
+  @Input() eslogan:string="";
+  @Input() logoONG:string="";
 }
