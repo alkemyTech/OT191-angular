58356098edb642b69c8fe57dc700665f66622af4
Daniel-Mdn <danielmedina012@gmail.com>
2022-05-16 21:31:12 -0300

    don't show button if logged in added

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/features/public/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  
+  loggedIn:boolean=false;
+  constructor(private authService: AuthService) { 
+    authService.verifyAuth().subscribe((res)=>{
+      this.loggedIn=res.success;})
+  }
 
   ngOnInit(): void {
   }
