134aaacf5579899b3f353b95cd0b4bbd11b01ae4
stefanoracke <stefanoracke@gmail.com>
2022-05-20 12:44:35 -0300

    fixing

--- src/app/features/backoffice/pages/backoffice-home/backoffice-home.component.ts
+++ src/app/features/backoffice/pages/backoffice-home/backoffice-home.component.ts
@@ -13,7 +13,7 @@ export class BackofficeHomeComponent {
     { name: "actividades", url: "/backoffice/activities", img:"../../../../../assets/svg/activities.svg" },
     { name: "categorias", url: "/backoffice/categories", img:"../../../../../assets/svg/categories.svg" },
 	{ name: "slides", url: "/backoffice/slides", img:"../../../../../assets/svg/slides.svg" },
-	{ name: "organización", url: "/backoffice/organization", img:"../../../../../assets/svg/organization.svg" },
+	{ name: "organización", url: "/backoffice/organization/edit", img:"../../../../../assets/svg/organization.svg" },
 	{ name: "home edition", url: "/backoffice/home", img:"../../../../../assets/svg/home.svg" },
 	{ name: "usuarios", url: "/backoffice/users", img:"../../../../../assets/svg/users.svg" },
   ];
